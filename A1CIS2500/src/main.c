9a74a8f235a852d3bcb682f4297745af90321cd7
dominickhera <dominickhera@Dominicks-MacBook-Pro.local>
2016-01-17 01:44:02 -0500

    making sure main open thing works first, not done yet

--- A1CIS2500/src/main.c
+++ A1CIS2500/src/main.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
 int main(){
+    FILE * openData;
+    FILE * openTemplate;
+    FILE * outputFile;
+    char line[256];
+
+    openTemplate = fopen("template.txt", "r");
+    if (openTemplate == NULL){
+        printf("couldn't open template text file\n");
+        return 1;
+    }
+/*
+    outputFile = fopen("outputFile.txt", "w");
+
+    while(fgets(line, 256, openTemplate) != NULL){
+        int length = 0;
+
+        length = strlen(line);
+        line[length-1] = '\0';
+
+        for (int i = 0; i < length; i++){
+            if(line[i] == 'o') {
+                line[i] = 'z';
+            }
+        }
+
+        fprintf(outputFile, "%s\n", line);
+
+    }
+*/
+fclose(openData);
+fclose(outputFile);
+fclose(openTemplate);
+
 
 return 0;
 }
