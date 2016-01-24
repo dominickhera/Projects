f095197e1153bc61c1672b61a7afb4404fa47d7f
dominickhera <dominickhera@Dominicks-MacBook-Pro.local>
2016-01-24 17:23:21 -0500

    now copies storeVal stuff

--- A1CIS2500/src/main.c
+++ A1CIS2500/src/main.c
@@ -5,11 +5,13 @@ int main(int argc, char* argv[]) {
     FILE * openData;
     FILE * openTemplate;
     FILE * outputFile;
-    char word[50];
     char line[256];
-    char variableArray[50];
-    int i = 0;
+    char value[256];
+    char key[256];
     int length = 0;
+    int argNum = 0;
+    int argLength = 0;
+    int sucNum = 0;
 
     if (argc != 4) {
         printf(" incorrect number of arguments given\n");
@@ -32,24 +34,41 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    while(fgets(line, 256, openData ) != NULL){
+    while(fgets(line, 256, openData) != NULL){
 
         length = strlen(line);
-        line[length-1] = '\0';
+        line[length+1] = '\0';
 
         for (int i = 0; i < length; i++) {
-             while(line[i] == '=') {
-		
-                            }
+            while(line[i] == '=') {
+                argLength = i;
+                i++;
+                argNum++;
+            }
         }
 
-        setValue(variableArray, line);
-        printf(getValue(variableArray));
-        fprintf(outputFile, "%s\n", line);
-    }
+	strncpy(key, line, argLength);
+	strcpy(value, line + (argLength + 1));
+	printf("Key:%s\nValue:%s\n", key, value);
+	sucNum = setValue(key, value);
+	memset(key, 0, length);
+}
+	if (sucNum == 1) {
+	printf("set success\nnumber of variables copied: %d\n", argNum);
+	}    
 
+	while(fgets(line, 256, openTemplate) != NULL) {
+	length = strlen(line);
+	line[length - 1] = '\0';
+
+	for (int i = 0; i < length; i++) {
+	if (line[i] == key) {
+	line[i] = getValue(key);
+	}
+    }
+}
     fclose(openTemplate);
     fclose(openData);
     fclose(outputFile);
     return 0;
-}
+ }
