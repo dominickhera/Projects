49c28b7192af0087e6f5eef967e9128fd7ce475d
dominickhera <dominickhera@Dominicks-MacBook-Pro.local>
2016-02-02 17:13:28 -0500

    playing with test file

--- rogue/src/test.c
+++ rogue/src/test.c
@@ -1,72 +1,71 @@
-#include<stdlib.h>
-#include<ncurses.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
 
 void printRec(int y, int x, int h, int w);
+
 void printMsg(const char* msg);
 
-int main(){
-
+int main() {
     int rows;
     int cols;
     char input[5];
     int tall;
     int wide;
 
-    initscr();
-    getmaxyx(stdscr, rows, cols);
+    initscr(); //initalize ncurses
+
+    getmaxyx(stdscr, rows, cols); //get size of screen
     printw("Screen size is %dX%d", rows, cols);
 
-    mvprintw(2, 3, "enter the height: ");
-    getstr(input);
+    //Move to position, and print
+    mvprintw(2, 3, "Enter the height: ");
+    getstr(input); //get string from user, store in input
     tall = (int)strtol(input, NULL, 10);
-    move(2, 0);
-    clrtoeol(); //clears line
+    move(2,0); //move to beginning of line
+    clrtoeol(); //clear line
+
 
     mvprintw(2, 3, "Enter the width: ");
     getstr(input);
     wide = (int)strtol(input, NULL, 10);
 
-    erase();
-    /*
-       for (int i = 0; i < wide; i++) {
-       mvaddch(0, i, '_');
-       }
-
-       for (int i=0; i <tall; i++) {
-       mvaddch(i, 0, '|');
-       mvaddch(i, wide, '|');
-       }
-
-       for (int i=0; i <= wide; i++) {
-       mvaddch(tall, i, '_');
-       }*/
+    erase(); //clear entire screen
 
     printRec(2,3,tall,wide);
+    getch();
     printRec(6,7,tall,wide);
+    getch();
     printRec(12,23,tall,wide);
-
     getch();
 
-    endwin();
+    /* Pause for input.
+       This really does nothing. It's just hear so you can see the screen
+       before the program exits. Hit any key to continue*/
+    printMsg("End of program. Push any key to quit.");
+    getch();
+
+
+    /* Important. Don't forget to do this: */
+    endwin(); //deinitalize ncurses
 
     return 0;
 }
 
 void printRec(int y, int x, int h, int w) {
-    for (int i=x; i < w; i++){
-        mvaddch(x, i, '#');
-    }
-    for (int i=0; i <h+y; i++) {
-        mvaddch(i, y, '#');
-        mvaddch(i, w+x , '#');
-    }
-    for ( int i =x; i <=w+x; i++){
-        mvaddch(h+y, i, '#');
+    for (int i=x; i<w+x; i++) {
+        mvaddch(y, i, '#');
     }
 
-printMsg("printed one rectangle");
-getch();
+    //THIS IS THE BUGGY LOOP
+    for (int i=y; i<h+y; i++) {
+        mvaddch(i, y, '#');
+        mvaddch(i, w+x, '#');
+    }
+
+    for (int i=x; i<=w+x; i++) {
+        mvaddch(h+y, i, '#');
+    }
+    printMsg("Printed one rectangle");
 }
 
 void printMsg(const char* msg) {
