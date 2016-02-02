08637054ab88524b48a156e9c4a007cff13bbad0
Dominick <dominickhera@me.com>
2016-02-01 21:11:59 -0500

    set up folders, starting to parse files and create the 6 required functions

---
+++ rogue/include/main.h
@@ -0,0 +1,9 @@
+#ifndef _USERNAME_HEADER_
+#define _USERNAME_HEADER_
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ncurses.h>
+
+#endif
+
---
+++ rogue/src/main.c
@@ -0,0 +1,37 @@
+#include"main.h"
+
+void endGame();
+void startGame();
+
+int main(int argc, char* argv[]) {
+/*
+    if  (argc != 1) {
+        printf("incorrect amount of arguments given\n");
+        return 1;
+    }*/
+
+startGame();
+/*
+    initscr();
+
+    printw("hello world");
+
+    refresh();
+
+    getch();*/
+
+    endGame();
+
+    return 0;
+}
+
+void startGame(){
+initscr();
+printw("hello world");
+refresh();
+getch();
+}
+
+void endGame(){
+    endwin();
+}
