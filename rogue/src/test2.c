9fc43b4a63c9fa9df81677a6c9442c57227f8aa0
Dominick <dominickhera@me.com>
2016-02-09 01:40:53 -0500

    more progress on parsing in test 2

--- rogue/src/test2.c
+++ rogue/src/test2.c
@@ -2,31 +2,58 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct roomStruct {
+    int x;
+    int y;
+    int width;
+    int height;
+}room;
+/*
+enum itemsEnum {
+    z,
+    a,
+    g,
+    G,
+    w,
+    W,
+    m,
+    e,
+    M,
+    P,
+};*/
+
 int main (int argc, char* argv[])
 {
-   FILE * openLevel;
+    FILE * openLevel;
     char text[200];
-    //char str[900];
+    room x[6];
+    room y[6];
+    room width[6];
+    room height[6];
+   // enum itemsEnum items;
     openLevel = fopen(argv[1], "r");
 
-    if (argc != 1){
-        printf("not enough arguments provided\n");
-        return 1;
+    if (argc != 2){
+      printf("not enough arguments provided\n");
+      return 1;
+      }
+
+    if (openLevel == NULL){
+        printf("couldn't open file\n");
     }
 
-    for(int i=0; ferror(openLevel) == 0; i++){
-        fscanf(openLevel, "%s", text);
-    	printf("%s\n", text);
-    }
-    //char str[] ="10X16 de4 dw9 ds8 g8,7 M3,4 h6,5 p2,2";
-    // str = text;
-    char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n",text);
-    pch = strtok (text," X de dw ds g , M h p");
-    while (pch != NULL)
+    while (fscanf(openLevel, "%s", text) != EOF)
     {
-        printf ("%s\n",pch);
-        pch = strtok (NULL, " X de dw ds g , M h p");
+        char * pch;
+        pch = strtok (text," ");
+        //pch = strtok (text," X de dw ds g , M h p");
+
+        while (pch != NULL)
+        {
+            printf ("%s\n",pch);
+           // pch = strtok (NULL, " X de dw ds g , M h p");
+            pch = strtok (NULL, " ");
+        }
     }
     return 0;
 }
