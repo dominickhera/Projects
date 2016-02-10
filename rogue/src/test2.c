041dd0b17aa5ccb8c5f558f523dac92c10e8f799
Dominick <dominickhera@me.com>
2016-02-10 03:14:32 -0500

    might be able to parse soon

--- rogue/src/test2.c
+++ rogue/src/test2.c
@@ -9,18 +9,18 @@ typedef struct roomStruct {
     int height;
 }room;
 /*
-enum itemsEnum {
-    z,
-    a,
-    g,
-    G,
-    w,
-    W,
-    m,
-    e,
-    M,
-    P,
-};*/
+   enum itemsEnum {
+   z,
+   a,
+   g,
+   G,
+   w,
+   W,
+   m,
+   e,
+   M,
+   P,
+   };*/
 
 int main (int argc, char* argv[])
 {
@@ -31,42 +31,32 @@ int main (int argc, char* argv[])
     room y[6];
     room width[6];
     room height[6];
-   // enum itemsEnum items;
+    // enum itemsEnum items;
     openLevel = fopen(argv[1], "r");
 
     if (argc != 2){
-      printf("not enough arguments provided\n");
-      return 1;
-      }
+        printf("not enough arguments provided\n");
+        return 1;
+    }
 
     if (openLevel == NULL){
         printf("couldn't open file\n");
     }
-roomThing = malloc(sizeof(room));
     while (fscanf(openLevel, "%s", text) != EOF)
     {
         char * pch;
-        pch = strtok (text," ");
-        //roomThing->x = atoi(pch);
-        //pch = strtok (text, "X");
-        //roomThing->y = atoi(pch);
-       // printf("%d", roomThing->x);
-        //pch = strtok (text," X de dw ds g , M h p");
-		//printf("%d", roomThing->y);
+        pch = strtok (text," X");
+
         while (pch != NULL)
         {
+
             printf ("%s\n",pch);
-           // pch = strtok (NULL, " X de dw ds g , M h p");
             pch = strtok (NULL, " ");
-          //  roomThing->y = atoi(pch);
-            //printf("%d", roomThing->y);
         }
-   roomThing->x = atoi(pch);
-    roomThing->y = atoi(pch);
+
     }
-  //  roomThing->x = atoi(pch);
-    //roomThing->y = atoi(pch);
-printf("%d", roomThing->x);
-printf("%d", roomThing->y);
+
+    roomThing = malloc(sizeof(room));
+    
     return 0;
 }
