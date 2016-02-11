d2130239b86453d46e4adeb9a58d6b705177962f
Dominick <dominickhera@me.com>
2016-02-11 04:07:06 -0500

    working on some parsing again, not much progress

--- rogue/src/parse.c
+++ rogue/src/parse.c
@@ -10,25 +10,26 @@ roomThing = malloc(sizeof(room));
 
     openLevel = fopen(argv[1], "r");
    
-   while(fgets(line, 900, openLevel) != NULL) {
+   while(fgets(line, 256, openLevel) != NULL) {
         int counter = 0;
         length = strlen(line);
         line[length - 1] = '\0'
+    }
+/*
+    roomThing->x = xInt;
+    roomThing->y = yInt;
+    roomThing->width = widthInt;
+    roomThing-height = heighInt;
+*/
+    return roomThing;
 
-            for (int i = 0; i < length; i++) {
+}
+
+
+for (int i = 0; i < length; i++) {
                 while(line[i] != 'X') {
                     word[wordLength] = line[i];
                     wordLength++;
                     i++;
                 }
             }
-    }
-
-    roomThing->x = xInt;
-    roomThing->y = yInt;
-    roomThing->width = widthInt;
-    roomThing-height = heighInt;
-
-    return roomThing;
-
-}
--- rogue/src/test2.c
+++ rogue/src/test2.c
@@ -57,6 +57,6 @@ int main (int argc, char* argv[])
     }
 
     roomThing = malloc(sizeof(room));
-    
+
     return 0;
 }
