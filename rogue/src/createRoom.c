43f52d71e49248a5817e8b58f5209a896e4ac8ef
dominickhera <dominickhera@Dominicks-MacBook-Pro.local>
2016-02-11 20:36:52 -0500

    creating room thing

--- rogue/src/createRoom.c
+++ rogue/src/createRoom.c
@@ -1,17 +1,18 @@
+
 char ** createRoom(int roomY, int roomX){
 
-char ** currentRoom;
+    char ** currentRoom;
 
-currentRoom = malloc(sizeof(char*) *roomY);
+    currentRoom = malloc(sizeof(char*) *roomY);
     if(currentRoom == NULL){
         printf("failed to allocate memory for room\n");
         exit(1);
     }
 
     for(int i=0;i<roomY;i++){
-        currentRoom[i] = malloc(sizeof(char*) *roomX);
+        currentRoom[i] = malloc(sizeof(char) *roomX);
         if(currentRoom[i] == NULL){
-            printf("failed to allocate memory for \n");
+            printf("failed to allocate memory for room \n");
             exit(1);
         }
     }
@@ -21,31 +22,24 @@ currentRoom = malloc(sizeof(char*) *roomY);
     }
 
     for(int i=0;i<roomX;i++){
-        currentRoom[roomY-1] [i] = '#';
+        currentRoom[roomY - 1] [i] = '#';
     }
 
     for(int i=0;i<roomY;i++){
-        currentRoom[0][i] = '#';
+        currentRoom[i] [0] = '#';
     }
 
     for(int i=0;i<roomY;i++){
-        currentRoom[roomX-1] [i] = '#';
+        currentRoom[i] [roomX - 1] = '#';
     }
 
     //floor
-    for(int i=1;i<roomY;i++){
+    for(int i=1;i<roomY-1;i++){
         for(int k=1;k<roomX-1;k++){
             currentRoom[i][k] = '.';
         }
     }
 
-    for(int i=0;i<roomY;i++){
-        printf("%s\n", currentRoom[i]);
-    }
-
-    for(int i=0;i<roomY;i++){
-        free(currentRoom[i]);
-    }
 
     return currentRoom;
 }
