fafb8fcb1912282d916d6f0d8cfb0e0ef6a1cfc8
Dominick <dominickhera@me.com>
2016-10-16 01:34:17 -0400

    deleted the functions codes from the main q2.c file, now that it's in its' own file

--- cis2520a2/src/q2.c
+++ cis2520a2/src/q2.c
@@ -4,46 +4,9 @@
 #include <ctype.h>
 #include "q2Stack.h"
 
-/*typedef struct Stack 
-{
-    float info;
-    struct Stack * next;
-
-}Stack;
-
-void push(Stack ** top, float input)
-{
-	Stack * new = malloc(sizeof(Stack));
-	if (!new) exit(-1);
-	new->info = input;
-	new->next = *top; 
-	*top = new; 
-	printf("=");
-}
-
-float pop(Stack** top)
-{
-	float tempData = 0.0;
-
-	Stack* temp;
-
-	if(temp == NULL)
-	{
-		printf("nothing is on the stack.\n");
-		return 0.0;
-	}
-
-	temp = *top;
-	*top = temp->next;
-	tempData = temp->info;
-	free(temp);
-
-	return tempData;
-}*/
-
 int main(int argc, char* argv[])
 {
-	// struct mainStruct * stack;
+
 	Stack * stack; 
 
 
