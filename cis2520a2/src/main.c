1bed3d6ce1e58e1f1b981565c21e9704922ea306
Dominick Hera <dominickhera@Dominicks-MacBook-Pro.local>
2016-10-16 16:53:21 -0400

    more work on Q1, gonna fail this definitely

--- cis2520a2/src/main.c
+++ cis2520a2/src/main.c
@@ -4,105 +4,121 @@
 // #include "avaliable.h"
 // #include "rented.h"
 // #include "repair.h"
-typedef struct Available *ptr;
+
+
 typedef struct Available
 {
-    char * availablePlateNum;
-    int availableMileage;
-    struct Available * next;
-}Available;
+	int availableMileage;
+	char * availablePlateNum;
+	struct Available * next;
+} node;
 
-
-// Available * addToAvaliableList(Available *head, int mileage){
-//     Available new;
-//     new = *head;
-//     Available T = (Available)malloc(sizeof(Available));
-
-//     T->availableMileage = mileage;
-//     while (new->next)
-//     {
-//     	new = new->next;
-//     }
-//     new->next = T;
-//     // new = list_nodes(mileage);
-//     // head->next = new;
-//     // printf("%d\n%d\n", new->availableMileage, new);
-//     // return(new);
-// }
-
-void addToAvaliableList(ptr *head, int mileage)
+typedef struct list
 {
-	while(*head)
+	node * next;
+}list;
+
+typedef struct Top
+{
+	list * listHead;
+}head;
+
+
+node * createNewNode(int miles, char * plate)
+{
+	node * newNode = malloc(sizeof(*newNode)); 
+	newNode->availablePlateNum = malloc(sizeof(char) * (strlen(plate)) + 1);
+	strcpy(newNode->availablePlateNum, plate);
+	newNode->availableMileage = miles;
+
+	return newNode; 	
+}
+
+void addtoAvailableList(node** head, int mileage, char * plate )
+{
+	node dummy, *new, *p;
+
+	new = createNewNode(mileage, plate);
+	p = &dummy;
+	dummy.next = *head;
+
+	while(p->next && mileage > p->next->availableMileage)
 	{
-		head = &(*head)->next;
-	}
-	*head = malloc(sizeof(**head));
-	(*head)->availableMileage = mileage;
-	(*head)->next = NULL;
+		p = p->next;
+	} 
+
+	new->next = p->next;
+	p->next = new;
+	*head = dummy.next;
 
 }
 
+// void printList(node* head)
+// {
+//     while (head)
+//     {
+//         printf("Plate Num: %s \nMileage: %d\n", head->availablePlateNum, head->availableMileage);
+//         head=head->next;
+//     }
+// }
 
 int main()
 {
-    // char * file = argv[1];
-	Available * head;
 
-    int inputNum = 0;
-    char availablePlateNumString[256];
-    int availableMileageInt = 0;
+	node * head;
 
-    while (inputNum != 7)
-    {
-        printf("(1) add a new car to the avaliable-for rent list\n(2) add a returned car to the avaliable-for-rent list\n(3) add a returned car to the repair list\n(4) transfer a car from the repair list to the avaliable-for-rent list\n(5) rent the first avaliable car\n(6) print all the lists\n(7) quit\n\n");
-        scanf("%d", &inputNum);	
-        switch(inputNum)
-        {
-            case 1:
-            	head = malloc(sizeof(Available));
-            	// char avaliablePlateNumString[256];
-            	// int avaliableMileageInt = 0;
-                printf("Enter the Plate Number of the car: ");
-                scanf("%s", availablePlateNumString);
-                printf("inputed string = %s", availablePlateNumString);
-                printf("\nEnter the mileage of the car: ");
-                scanf("%d", &availableMileageInt);
-                printf("inputted mileage = %d\n", availableMileageInt);
+	int inputNum = 0;
+	char availablePlateNumString[256];
+	int availableMileageInt = 0;
 
-                addToAvaliableList(&head, availableMileageInt);
-                printf("%d is the mileage\n", head->availableMileage);
+	while (inputNum != 7)
+	{
+		printf("(1) add a new car to the avaliable-for rent list\n(2) add a returned car to the avaliable-for-rent list\n(3) add a returned car to the repair list\n(4) transfer a car from the repair list to the avaliable-for-rent list\n(5) rent the first avaliable car\n(6) print all the lists\n(7) quit\n\n");
+		scanf("%d", &inputNum);	
+		switch(inputNum)
+		{
+			case 1:
 
-                // addToAvaliableList("A", avaliablePlateNumString, avaliableMileageInt);
+				printf("Enter the Plate Number of the car: ");
+				scanf("%s", availablePlateNumString);
+				printf("Enter the mileage of the car: ");
+				scanf("%d", &availableMileageInt);
+				printf("\n");
+				addtoAvailableList(&head, availableMileageInt, availablePlateNumString);
+				// addtoAvailableList(&H, availableMileageInt, availablePlateNumString);
+				printf("%s is the available plate num\n%d is the mileage\n\n", head->availablePlateNum, head->availableMileage);
 
-                break;
-            case 2:
-                printf("you pressed 2\n");
-                break;
-            case 3:
-                printf("you pressed 3\n");
-                break;
-            case 4:
-                printf("you pressed 4\n");
-                break;
-            case 5:
-                printf("you pressed 5\n");
-                break;
-            case 6:
-                // printAvaliableList();
-                // printRentedList();
-            printf("you pressed 6\n");
-                break;
-            case 7:
-                printf("goodbye\n");
-                break;
-            default:
-                printf("please enter a number between 1 - 7\n");
-                break;
-        }
-    }
+				break;
+			case 2:
+				// printf("you pressed 2\n");
+				// printList(H);
+				// printList(&head);
+				break;
+			case 3:
+				printf("you pressed 3\n");
+				break;
+			case 4:
+				printf("you pressed 4\n");
+				break;
+			case 5:
+				printf("you pressed 5\n");
+				break;
+			case 6:
+				// printAvaliableList();
+				// printRentedList();
+				printf("you pressed 6\n");
+				break;
+			case 7:
+				printf("goodbye\n");
+				break;
+			default:
+				printf("please enter a number between 1 - 7\n");
+				break;
+		}
+	}
 
 
-    printf("call print all lists function here\n");
+	printf("call print all lists function here\n");
 
-    return 0;
+	return 0;
 }
