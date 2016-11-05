92dc8561abfd5dda41348ccee44e6b7d29037172
Dominick <dominickhera@me.com>
2016-11-05 19:40:50 -0400

    meh

--- cis2520a3/src/q1.c
+++ cis2520a3/src/q1.c
@@ -2,58 +2,141 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum TYPE
+{
+    OPERATOR,
+    VARIABLE,
+    VALUE,
+    DEFAULT
+};
+
+typedef struct treeNode
+{
+    struct treeNode * leftBranch;
+    struct treeNode * rightBranch;
+    float num;
+    char operator;
+    char nodeID;
+    enum TYPE type;
+
+}treeNode;
+
+
+int operatorCheck(char operator)
+{
+    if (operator == '*' || operator == '/' || operator == '-' || operator == '+')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+treeNode * createHead()
+{
+	treeNode * new = malloc(sizeof(treeNode));
+	new->rightBranch = NULL;
+	new->leftBranch = NULL;
+
+	return new;
+}
+
+treeNode * parseExpression(char * expression)
+{
+    // char expressionInput[256];
+    int bracketCount = 0;
+    // int subCount = 0;
+    // int otherSubCount = 0;
+    int newSplit = 0;
+    int length = strlen(expression) - 1;
+    int actualLength = strlen(expression);
+
+    // printf("%s is %d chracters long\n", expression, length);
+
+    for (int i = 0; i < length; i++)
+    {
+
+        if(expression[i] == '(')
+        {
+            bracketCount++;
+        }
+
+        if(expression[i] == ')')
+        {
+            bracketCount--;
+        }
+
+        if(bracketCount == 1 && operatorCheck(expression[i]) == 1 )
+        {
+            newSplit = i;
+
+            char * leftBranch = malloc(sizeof(char)*256);
+            memset(leftBranch, '\0', 256);
+            char * rightBranch = malloc(sizeof(char)*256);
+            memset(rightBranch, '\0', 256);
+
+            strncpy(leftBranch, &expression[1], newSplit - 1);
+            strncpy(rightBranch, &expression[newSplit + 1], (actualLength - 2) - newSplit);
+
+            treeNode * new = createHead();
+            // while(subCount <= length)
+            // {
+            // 	strcpy(&expressionInput[otherSubCount], &expression[subCount]);
+            // 	subCount++;
+            // 	otherSubCount++;
+            // }
+            // break;
+        }
+    }
+
+}
+
 int main(int argc, char* argv[])
 {
-	// int userInput = 0;
-	// char expressionInput[256];
-	char * expression = argv[1];
-	int openBracketCount = 0;
-	int closeBracketCount = 0;
+    // int userInput = 0;
+    char * expression = argv[1];
+    treeNode * start = parseExpression(expression);
 
-	int length = strlen(expression);
 
-	printf("%s is %d chracters long\n", expression, length);
+    // printf("%s\n", expressionInput);
 
-	for (int i = 0; i < length; i++)
-	{
-		printf("%c\n", expression[i]);
-	}
+    // while (userInput != 7)
+    // {
 
-	// while (userInput != 7)
-	// {
+    // 	printf("(1) Display\n(2) Preorder\n(3) Inorder\n(4) Postorder\n(5) Update\n(6) Calclulate\n(7) Exit\n");
+    // 	scanf("%d", &userInput);
 
-	// 	printf("(1) Display\n(2) Preorder\n(3) Inorder\n(4) Postorder\n(5) Update\n(6) Calclulate\n(7) Exit\n");
-	// 	scanf("%d", &userInput);
+    // 	switch(userInput)
+    // 	{
+    // 		case 1:
+    // 			// printf("enter expression input:\n");
+    // 			// scanf("%s", expressionInput);
+    // 			// printf("%s is your input\n", expressionInput);
+    // 			break;
+    // 		case 2:
+    // 			printf("2\n");
+    // 			break;
+    // 		case 3:
+    // 			printf("3\n");
+    // 			break;
+    // 		case 4:
+    // 			printf("4\n");
+    // 			break;
+    // 		case 5:
+    // 			printf("5\n");
+    // 			break;
+    // 		case 6:
+    // 			printf("6\n");
+    // 			break;
+    // 		case 7:
+    // 			printf("goodbye\n");
+    // 			exit(1);
+    // 		default:
+    // 			printf("please enter a number between 1 and 7\n");
+    // 	}
+    // }
 
-	// 	switch(userInput)
-	// 	{
-	// 		case 1:
-	// 			// printf("enter expression input:\n");
-	// 			// scanf("%s", expressionInput);
-	// 			// printf("%s is your input\n", expressionInput);
-	// 			break;
-	// 		case 2:
-	// 			printf("2\n");
-	// 			break;
-	// 		case 3:
-	// 			printf("3\n");
-	// 			break;
-	// 		case 4:
-	// 			printf("4\n");
-	// 			break;
-	// 		case 5:
-	// 			printf("5\n");
-	// 			break;
-	// 		case 6:
-	// 			printf("6\n");
-	// 			break;
-	// 		case 7:
-	// 			printf("goodbye\n");
-	// 			exit(1);
-	// 		default:
-	// 			printf("please enter a number between 1 and 7\n");
-	// 	}
-	// }
-
-	return 0;
+    return 0;
 }
--- cis2520a3/src/q2.c
+++ cis2520a3/src/q2.c
@@ -60,19 +60,19 @@ int removeMin()
 
 int main(int argc, char* argv[])
 {
-	char * file = argv[1]; 
-	char line[256];
+    char * file = argv[1]; 
+    char line[256];
     int keyArray[256];
-	int numArray[20][10];
-	int i = 0;
-	int j = 0;
+    int numArray[20][10];
+    int i = 0;
+    int j = 0;
     int numCount = 0;
-	char *token;
-	const char space[2] = " ";
-	FILE *openFile;
-	openFile = fopen(file, "r");
+    char *token;
+    const char space[2] = " ";
+    FILE *openFile;
+    openFile = fopen(file, "r");
 
-	if(openFile == NULL)
+    if(openFile == NULL)
     {
         printf("Could not open %s\n", file);
         return 0;
@@ -80,40 +80,40 @@ int main(int argc, char* argv[])
 
     if (argc != 1)
     {
-    	printf("%s loaded\n", file);
+        printf("%s loaded\n", file);
 
     }
     else
-   	{
-    	printf("wrong numbah hoe\n");
+    {
+        printf("wrong numbah hoe\n");
     }
 
     while(fgets(line, 256, openFile))
     {
-    	// printf("%s", line);
-    	token = strtok(line, space);
-    	while(token != NULL)
-    	{
-    		if (strcmp(token,"\n") == 0)
-    		{
-    			j = 0;
-    			token = strtok(NULL, space);
-    			// printf("\n");
-    		}
-    		else
-    		{
+        // printf("%s", line);
+        token = strtok(line, space);
+        while(token != NULL)
+        {
+            if (strcmp(token,"\n") == 0)
+            {
+                j = 0;
+                token = strtok(NULL, space);
+                // printf("\n");
+            }
+            else
+            {
 
-    			numArray[i][j] = atoi(token);
-    			// printf( "numArray[%d][%d]: %d\n", i, j, numArray[i][j] );
-    			j++;
-    			if (j > 9)
-    			{
-    				i++;
-    				j = 0;
-    			}
-     			 token = strtok(NULL, space);
-     		}
-    	}
+                numArray[i][j] = atoi(token);
+                // printf( "numArray[%d][%d]: %d\n", i, j, numArray[i][j] );
+                j++;
+                if (j > 9)
+                {
+                    i++;
+                    j = 0;
+                }
+                token = strtok(NULL, space);
+            }
+        }
 
     }
 
@@ -128,7 +128,7 @@ int main(int argc, char* argv[])
                 // addToHeap(keyArray[i]);
                 // printf("%d is the num count for array [%d]\n", numCount, i);
             }
-            
+
         }
         addToHeap(keyArray[i]);
         printf("%d is the num count for array [%d]\n", keyArray[i], i);
@@ -137,10 +137,10 @@ int main(int argc, char* argv[])
 
     for (int i = 0; i < 20; i++)
     {
-        
+
         printf("%d ", removeMin());
     }
     printf("\n");
 
-	return 0;
+    return 0;
 }
