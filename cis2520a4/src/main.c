422b8161bc543213fc50a17379af97e789975476
Dominick <dominickhera@me.com>
2016-11-21 20:48:52 -0500

    just got main program finished, need to rename file and other stuff

--- cis2520a4/src/main.c
+++ cis2520a4/src/main.c
@@ -8,17 +8,22 @@ int main()
 
     int userInput = 0;
     char fileName[256];
-    char word[30];
-
+    char line[256];
+    
+    char frequencyCompare[10];
+    char keyRemove[25];
+    char keyInsert[25];
+    char keySearch[25];
+    
     while(userInput != 7)
     {
-        printf("1. Initialization\n2. Find\n3. Insert\n4. Remove\n5. Check Height and Size\n6. Find All (above a given frequency)\n7. Exit\n");
+        printf("1. Initialization\n2. Find\n3. Insert\n4. Remove\n5. Check Height and Size\n6. Find All (above a given frequency)\n7. Exit\n\navl/> ");
         scanf("%d", &userInput);
 
         switch(userInput)
         {
             case 1:
-                printf("enter file name: ");
+                printf("filename: ");
                 scanf("%s", fileName);
                 FILE *fp;
                 fp = fopen(fileName, "r");
@@ -29,25 +34,33 @@ int main()
                     return 0;
                 }
                 
-                while(fscanf(fp, "%s", word) != EOF)
+                while(fgets(line, sizeof(line), fp))
                 {
-                    printf("%s\n", word);
+                    printf("%s", line);
                 }
                 break;
             case 2:
-                printf("2\n");
+                printf("find key: ");
+                scanf("%s", keySearch);
+                printf("key: %s, frequency: someNumber\n", keySearch);
                 break;
             case 3:
-                printf("3\n");
+                printf("insert key: ");
+                scanf("%s", keyInsert);
+                printf("key: %s, frequency: someNumber\n", keyInsert);
                 break;
             case 4:
-                printf("4\n");
+                printf("remove key: ");
+                scanf("%s", keyRemove);
+                printf("key: %s, frequency: someNumber-1\n", keyRemove);
                 break;
             case 5:
-                printf("5\n");
+                printf("height : someNumber, size: someNumber\n");
                 break;
             case 6:
-                printf("6\n");
+                printf("frequency: ");
+                scanf("%s", frequencyCompare);
+                printf("key: someKey, frequency: someNumber\n");
                 break;
             case 7:
                 printf("goodbye!\n"); 
