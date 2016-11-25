8d9fd257ded815b13b8257d9becdbf22078bb2da
Dominick Hera <dominickhera@me.com>
2016-11-24 19:09:16 -0500

    parsing is working i guess? now i need to figure out how to add that to the tree/node

--- cis2520a4/src/avltree.c
+++ cis2520a4/src/avltree.c
@@ -19,6 +19,23 @@ struct avlTree
 
 typedef struct avlTree avlTreeInit;
 
+void initialize (FILE * fp)
+{
+    char line[256];
+    char * key;
+
+    while(fgets(line, sizeof(line), fp))
+    {
+            key = strtok(line, " ");
+            printf("%s\n", key);
+            while((key = strtok(NULL, " ")) != NULL)
+            {
+                printf("%s\n", key );
+            }   
+            memset(line, '\0', strlen(line));
+    }
+}
+
 avlTreeInit *createAVL()
 {
     avlTreeInit *tree = NULL;
@@ -27,8 +44,8 @@ avlTreeInit *createAVL()
         return NULL; 
     }
 
-   tree->avlRoot = NULL;
-   return tree; 
+    tree->avlRoot = NULL;
+    return tree; 
 }
 
 avlNodeInit *createAVLNode()
@@ -53,58 +70,58 @@ int main()
     char keyRemove[25];
     char keyInsert[25];
     char keySearch[25];
-    
+
     while(userInput != 7)
     {
         printf("1. Initialization\n2. Find\n3. Insert\n4. Remove\n5. Check Height and Size\n6. Find All (above a given frequency)\n7. Exit\n\navl/> ");
         scanf("%d", &userInput);
-      switch(userInput)
-      { 
-          case 1:
-              printf("filename: ");
-              scanf("%s", fileName); 
-              FILE *fp;
-              fp = fopen(fileName, "r");
+        switch(userInput)
+        { 
+            case 1:
+                printf("filename: ");
+                scanf("%s", fileName); 
+                FILE *fp;
+                fp = fopen(fileName, "r");
 
-              if(fp == NULL)
-              {
-                  printf("could not find file\n");
-                  return 0;
-              }
-
-              while(fgets(line, sizeof(line), fp))
-              {
+                if(fp == NULL)
+                {
+                    printf("could not find file\n");
+                    return 0;
+                }
+                initialize(fp);	
+                /*while(fgets(line, sizeof(line), fp))
+                  {
                   printf("%s", line);
-              }
-              break;
-          case 2:
-              printf("find key: ");
-              scanf("%s", keySearch);
-              printf("key: %s, frequency: someNumber\n", keySearch);
-              break;
-          case 3:
-              printf("insert key: ");
-              scanf("%s", keyInsert);
-              printf("key: %s, frequency: someNumber\n", keyInsert);
-              break;
-          case 4:
-              printf("remove key: ");
-              scanf("%s", keyRemove);
-              printf("key: %s, frequency: someNumber-1\n", keyRemove);
-              break;
-          case 5:
-              printf("height : someNumber, size: someNumber\n");
-              break;
-          case 6:
-              printf("frequency: ");
-              scanf("%s", frequencyCompare);
-              printf("key: someKey, frequency: someNumber\n");
-              break;
-          case 7:
-              printf("goodbye!\n"); 
-              exit(1);
-              break;
-      }
+                  }*/
+                break;
+            case 2:
+                printf("find key: ");
+                scanf("%s", keySearch);
+                printf("key: %s, frequency: someNumber\n", keySearch);
+                break;
+            case 3:
+                printf("insert key: ");
+                scanf("%s", keyInsert);
+                printf("key: %s, frequency: someNumber\n", keyInsert);
+                break;
+            case 4:
+                printf("remove key: ");
+                scanf("%s", keyRemove);
+                printf("key: %s, frequency: someNumber-1\n", keyRemove);
+                break;
+            case 5:
+                printf("height : someNumber, size: someNumber\n");
+                break;
+            case 6:
+                printf("frequency: ");
+                scanf("%s", frequencyCompare);
+                printf("key: someKey, frequency: someNumber\n");
+                break;
+            case 7:
+                printf("goodbye!\n"); 
+                exit(1);
+                break;
+        }
     }
 
 
