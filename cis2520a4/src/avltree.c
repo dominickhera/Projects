ff41f813af912c63c28e5764939e4368b9ac2f00
Dominick Hera <dominickhera@me.com>
2016-11-27 14:49:44 -0500

    no seg faults on init anymore, balancing works

--- cis2520a4/src/avltree.c
+++ cis2520a4/src/avltree.c
@@ -51,58 +51,144 @@ avlNodeInit *createAVLNode(char *avlNodeValue)
     return node;
 }
 
+int avlHeight(avlNodeInit *node)
+{
+
+    int rightBranchHeight = 0;
+    int leftBranchHeight = 0;
+    if(node->rightBranch)
+    {
+        rightBranchHeight = avlHeight(node->rightBranch);
+    }
+
+    if (node->leftBranch)
+    {
+        leftBranchHeight = avlHeight(node->leftBranch);
+    }
+
+    return rightBranchHeight > leftBranchHeight ? ++rightBranchHeight : ++leftBranchHeight;
+}
+
+int balanceTrack(avlNodeInit *node)
+{
+
+    int balanceTrackInt = 0;
+    if(node->rightBranch)
+    {
+        balanceTrackInt = balanceTrackInt - avlHeight(node->rightBranch);
+    }
+
+    if(node->leftBranch)
+    {
+        balanceTrackInt = balanceTrackInt + avlHeight(node->leftBranch);
+    }
+
+    return balanceTrackInt;
+
+}
+
 avlNodeInit * doubleRightRotation(avlNodeInit * node)
 {
-    avlNodeInit* a = node->leftBranch;
-    a->leftBranch = node->rightBranch;
-    node->rightBranch = a;
 
-    return a;
+    avlNodeInit *a = node;
+    avlNodeInit *b = a->rightBranch;
+    
+    a->rightBranch = b->leftBranch;
+    b->leftBranch = a;
+
+    return b;
 }
 
 avlNodeInit * doubleLeftRotation(avlNodeInit * node)
 {
-    avlNodeInit* a = node->rightBranch;
-    a->rightBranch = node->leftBranch;
-    node->leftBranch = a;
 
-    return a;
+    avlNodeInit *a = node;
+    avlNodeInit *b = a->leftBranch;
+
+    a->leftBranch = b->rightBranch;
+    b->rightBranch = a;
+
+    return b;
 }
 
 avlNodeInit * rightLeftRotation(avlNodeInit * node)
 {
 
-    avlNodeInit* a = node->leftBranch;
-    avlNodeInit* b = a->rightBranch;
-    node->leftBranch = b->rightBranch;
-    a->rightBranch = b->leftBranch;
-    b->leftBranch = a;
-    b->rightBranch = node;
+    avlNodeInit *a = node;
+    avlNodeInit *b = a->rightBranch;
+    avlNodeInit *c = b->leftBranch;
 
-    return b;
+    a->rightBranch = c->leftBranch;
+    b->leftBranch = c->rightBranch;
+    c->rightBranch = b;
+    c->leftBranch = a;
 
+    return c;
 }
 
 avlNodeInit * leftRightRotation(avlNodeInit * node)
 {
 
-    avlNodeInit* a = node->rightBranch;
-    avlNodeInit* b = a->leftBranch;
-    node->rightBranch = b->leftBranch;
-    a->leftBranch = b->rightBranch;
-    b->rightBranch = a;
-    b->leftBranch = node;
+    avlNodeInit *a = node;
+    avlNodeInit *b = a->leftBranch;
+    avlNodeInit *c = b->rightBranch;
 
-    return b;
+    a->leftBranch = c->rightBranch;
+    b->rightBranch = c->leftBranch;
+    c->leftBranch = b;
+    c->rightBranch = a;
+
+    return c;
 }
 
 
 avlNodeInit * balanceAVLNode (avlNodeInit *node)
 {
+    avlNodeInit * tempRoot = NULL;
 
-    printf("poop nuts\n");
+    if(node->rightBranch)
+    {
+        node->rightBranch = balanceAVLNode(node->rightBranch);
+    }
+    if(node->leftBranch)
+    {
+        node->leftBranch = balanceAVLNode(node->leftBranch);
+    }
+    int balanceTrackInt = balanceTrack(node);
 
-    return 0;
+    if(balanceTrackInt <= -2)
+    {
+        if(balanceTrack(node->rightBranch) >= 1)
+        {
+            printf("right rotation\n");
+            tempRoot = rightLeftRotation(node);
+        }
+        else
+        {
+            printf("double right \n");
+            tempRoot = doubleRightRotation(node);
+            printf("done with double right\n");
+        }
+    }
+    else if(balanceTrackInt >= 2)
+    {
+        if(balanceTrack(node->leftBranch) <= -1)
+        {
+            printf("left rotation\n");
+            tempRoot = leftRightRotation(node);
+        }
+        else
+        {
+            printf("double left\n");
+            tempRoot = doubleLeftRotation(node);
+        }
+    }
+    else
+    {
+        tempRoot = node;
+    }
+
+    return tempRoot;
 
 }
 
@@ -134,7 +220,6 @@ void insert(avlTreeInit *tree, char * avlNodeValue)
     else
     {
         next = tree->avlRoot;
-        // printf("1\n");
         while(next != NULL)
         {
             last = next;
@@ -148,34 +233,26 @@ void insert(avlTreeInit *tree, char * avlNodeValue)
             }
             else if(strcmp(avlNodeValue, next->avlNodeValue) == 0)
             {
-                printf("the frequency of %s is %d\n", last->avlNodeValue, next->frequency);
-               last->frequency++;
-               // node->frequency = next->frequency;
+                // printf("the frequency of %s is %d\n", last->avlNodeValue, last->frequency);
+                last->frequency++;
 
-               return;
+                return;
             }
-            // printf("15\n");
         }
-        // printf("16\n");
         node = createAVLNode(avlNodeValue);
-        // printf("17\n");
         strcpy(node->avlNodeValue, avlNodeValue);
-        // printf("18\n");
         if(strcmp(avlNodeValue, last->avlNodeValue) > 0)
         {
-            // printf("19\n");
             last->leftBranch = node;
             // printf("20\n");
         }
         if(strcmp(avlNodeValue, last->avlNodeValue) < 0)
         {
-            // printf("21\n");
             last->rightBranch = node;
         }
-        // printf("23\n");
     }
-// printf("24\n");
-    // balanceAVLTree(tree);
+    // printf("bouta balance fam\n");
+    balanceAVLTree(tree);
 }
 
 void initialize (FILE * fp)
@@ -187,13 +264,13 @@ void initialize (FILE * fp)
 
     while(fgets(line, sizeof(line), fp))
     {
-            key = strtok(line, " ");
+        key = strtok(line, " ");
+        insert(tree, key);
+        while((key = strtok(NULL, " ")) != NULL)
+        {;
             insert(tree, key);
-            while((key = strtok(NULL, " ")) != NULL)
-            {;
-                insert(tree, key);
-            }   
-            memset(line, '\0', strlen(line));
+        }   
+        memset(line, '\0', strlen(line));
     }
 }
 
@@ -215,7 +292,7 @@ int main()
         switch(userInput)
         { 
             case 1:
-            
+
                 printf("filename: ");
                 scanf("%s", fileName); 
                 FILE *fp;
@@ -227,6 +304,7 @@ int main()
                 }
                 // tree = createAVL();
                 initialize(fp);	
+                fclose(fp);
                 break;
             case 2:
                 printf("find key: ");
