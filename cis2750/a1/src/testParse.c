26a870f5e683cd1fa9641ca5da1fc642b5c2ec87
Dominick Hera <dominickhera@me.com>
2017-01-11 17:14:28 -0500

    more progress on parser

--- cis2750/a1/src/testParse.c
+++ cis2750/a1/src/testParse.c
@@ -4,28 +4,45 @@
 
 int main()
 {
-	char filename[256];
-	char line[256];
+    char filename[256];
+    char line[256];
+    char word[256];
 
-	printf("enter filename: ");
-	scanf("%s", filename);
+    printf("enter filename: ");
+    scanf("%s", filename);
 
-	FILE *fp;
+    FILE *fp;
+    char * stringThing;
 
-	fp = fopen(filename, "r");
+    fp = fopen(filename, "r");
 
-	if(fp == NULL)
-	{
-		printf("could not find file\n");
-		return 0;
-	}
+    if(fp == NULL)
+    {
+        printf("could not find file\n");
+        return 0;
+    }
 
-	while(fgets(line, sizeof(line), fp))
-	{
-		printf("%s", line);
+    while(fgets(line, sizeof(line), fp) != NULL)
+    {
 
-		memset(line, '\0', strlen(line));
-	}
+        if (line[strlen(line) - 1] == '\n')
+        {
+            line[strlen(line) - 1] = '\0';
+        }
+        stringThing = strtok(line, " ");
 
-	return 0;
+        while(stringThing != NULL)
+        {
+            printf("%s\n", stringThing);
+
+            stringThing = strtok(NULL, " ");
+        }
+
+        memset(line, '\0', strlen(line));
+
+    }
+
+    fclose(fp);
+
+    return 0;
 }
