a98dfff6c3a6bb708acc0266aed00ec07fbb4134
Dominick <dominickhera@me.com>
2017-01-17 00:11:05 -0500

    trying a different way to parse, tomorrow is bootcamp day

--- cis2750/a1/src/testParse.c
+++ cis2750/a1/src/testParse.c
@@ -7,12 +7,18 @@ int main()
     char filename[256];
     char line[256];
     char word[256];
+    char character[256];
+    int charCount;
+    int i = 0;
 
     printf("enter filename: ");
     scanf("%s", filename);
 
     FILE *fp;
     char * stringThing;
+
+
+    stringThing = malloc(1000);
 
     fp = fopen(filename, "r");
 
@@ -22,25 +28,42 @@ int main()
         return 0;
     }
 
-    while(fgets(line, sizeof(line), fp) != NULL)
+    while((charCount = fgetc(fp)) != EOF)
     {
 
-        if (line[strlen(line) - 1] == '\n')
-        {
-            line[strlen(line) - 1] = '\0';
-        }
-        stringThing = strtok(line, " ");
+    	stringThing[i++] = (char) charCount;
 
-        while(stringThing != NULL)
-        {
-            printf("%s\n", stringThing);
-
-            stringThing = strtok(NULL, " ");
-        }
-
-        memset(line, '\0', strlen(line));
-
+    	if(charCount == '{')
+    	{
+    		printf("found a bracket\n");
+    	}
+    	else
+    	{
+    	printf("%c\n", charCount);
+    	}
     }
+
+    stringThing[i] = '\0';
+
+    // while(fgets(line, sizeof(line), fp) != NULL)
+    // {
+
+    //     if (line[strlen(line) - 1] == '\n')
+    //     {
+    //         line[strlen(line) - 1] = '\0';
+    //     }
+    //     stringThing = strtok(line, " ");
+
+    //     while(stringThing != NULL)
+    //     {
+    //         printf("%s\n", stringThing);
+
+    //         stringThing = strtok(NULL, " ");
+    //     }
+
+    //     memset(line, '\0', strlen(line));
+
+    // }
 
     fclose(fp);
 
