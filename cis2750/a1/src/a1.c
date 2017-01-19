84f682764930c6b3f10351bbaee731bd9f902023
Dominick Hera <dominickhera@me.com>
2017-01-19 14:13:33 -0500

    working on moving the functions outside of structs

--- cis2750/a1/src/a1.c
+++ cis2750/a1/src/a1.c
@@ -6,9 +6,17 @@ int main()
 {
     char filename[256];
     char line[256];
-    int length;
+    int structOpen = 0;
+    int functionOpen = 0;
     char * classReplace;
+    char * structCheck;
+    char * functionCheck;
+    char * openBracketCheck;
+    char * mainCheck;
     char buffer[500];
+    char lineBuffer[500];
+    char lineSave[500]; 
+    char **functionArray;
 
     printf("enter filename: ");
     scanf("%s", filename);
@@ -31,14 +39,43 @@ int main()
             line[strlen(line) - 1] = '\0';
         }
 
-        length = strlen(line);
-
-        if((classReplace = strstr(line, "class ")))
+        if((classReplace = strstr(line, "class ")) && (structCheck = strstr(line, "{")))
         {
-            strncpy(buffer, line, classReplace-line);
-            buffer[classReplace-line] = 0;
-            sprintf(buffer+(classReplace-line), "%s%s", "struct ", classReplace + strlen("class "));
+            strncpy(buffer, line, classReplace - line);
+            buffer[classReplace - line] = 0;
+            sprintf(buffer + (classReplace - line), "%s%s", "struct ", classReplace + strlen("class "));
             printf("%s\n", buffer);
+            if((structCheck = strstr(line, "{")))
+            {
+                // printf("fam this is the start of a struct\n");
+                structOpen++;
+                // printf("struct open is %d\n", structOpen);
+            }
+        }
+        else if((structCheck = strstr(line, ") {")) && (!(mainCheck = strstr(line, "int main"))) && (openBracketCheck = strstr(line,"("))  &&  (structOpen <= 1))
+        {
+            strcpy(lineSave, line); 
+            strncpy(lineBuffer, line, structCheck - line);
+            lineBuffer[structCheck - line] = 0;
+            sprintf(lineBuffer + (structCheck - line), "%s%s", "();", structCheck + strlen(") {"));
+            printf("%s\n", lineBuffer);
+            printf("the saved line is %s\n", lineSave);
+            structOpen++;
+            //printf("function inside a struct fam & structOpen is %d ", structOpen);
+            //printf("%s\n", line);
+
+        }
+        else if((structCheck = strstr(line, "{")))
+        {
+            structOpen++;
+            // printf("struct open is %d ", structOpen);
+            printf("%s\n", line);
+        }
+        else if((structCheck = strstr(line, "}")))
+        {
+            structOpen--;
+            // printf("struct open is %d ", structOpen);
+            printf("%s\n", line);
         }
         else
         {
