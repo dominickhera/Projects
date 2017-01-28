d0b47b68cf4bbb4e8b144a2ddca499d47ee5acc0
Dominick <dominickhera@me.com>
2017-01-28 16:52:09 -0500

    program now infinite loops, time to go back and figure out hwat i did wrong

--- cis2750/a1/src/test.c
+++ cis2750/a1/src/test.c
@@ -40,53 +40,51 @@ int main()
     int variableFindCount = 0; 
     int count = 0;   
     int functionStorageCount = 0; 
- 
-    for(int i = 0; i < 500; i++)
+    int i;
+/*
+    for(i = 0; i < 500; i++)
     {
         functionArray[i] = malloc(sizeof(char) * 500);
     }
- 
+*/
     printf("enter file name: ");
     scanf("%s", fileName);
 
     fp = fopen(fileName, "r"); 
-    
+
     if(fp == NULL)
     {
         printf("could not find file\n");
         return 0;
     }
-    
+
     while(fgets(line, sizeof(line), fp) != NULL)
     {
         strcpy(fileStorage[count], line); 
         count++;
     }    
 
-    for(int i = 0; i < count; i++)
+    for(i = 0; i < count; i++)
     {
         if((classCheck = strstr(fileStorage[i], "class ")) && (!(commentCheck = strstr(fileStorage[i], "//"))))
         {
-           strncpy(buffer, fileStorage[i], classCheck - fileStorage[i]);
-           buffer[classCheck - fileStorage[i]] = 0;
-           sprintf(buffer + (classCheck - fileStorage[i]), "%s%s", "struct ", classCheck + strlen("class "));
-           printf("line[%d]: %s", i, buffer); 
-           if((structCheck = strstr(fileStorage[i], "{")))
-           {
-            structOpen++;
-           }
+            strncpy(buffer, fileStorage[i], classCheck - fileStorage[i]);
+            buffer[classCheck - fileStorage[i]] = 0;
+            sprintf(buffer + (classCheck - fileStorage[i]), "%s%s", "struct ", classCheck + strlen("class "));
+            printf("line[%d]: %s", i, buffer); 
+            if((structCheck = strstr(fileStorage[i], "{")))
+            {
+                structOpen++;
+            }
         }
-        else if((structCheck = strstr(fileStorage[i], ") {")) && (!(mainCheck = strstr(fileStorage[i], "int main"))) && (structOpen = 1))
+        else if((structCheck = strstr(fileStorage[i], ") {")) && (!(mainCheck = strstr(fileStorage[i], "int main"))))
         {
-            strcpy(store, fileStorage[i]);
-            functionOpen++;
             variableLength = 0;
             variableFindCount = 0;
             variableSize = 0;
             strcpy(variableStore, "");
             strcpy(variableStore, fileStorage[i]);
-            strcpy(store, variableStore);
-            for(int i = 0; i < strlen(variableStore); i++)
+            for(i = 0; i < strlen(variableStore); i++)
             {
                 if(variableStore[i] == '(')
                 {
@@ -100,9 +98,6 @@ int main()
                 } 
             }
 
-            //printf("variables: %s for line[%d]\n", finalVariable, i);
-
-
             if((intCheck = strstr(finalVariable, "int ")))
             {
                 intCount = 0;
@@ -111,7 +106,6 @@ int main()
                     intCheck = strstr(intCheck + 1, "int ");
                     intCount++; 
                 }
-                //printf("intCount: %d for line[%d]\n", intCount, i);
             }
 
             if((floatCheck = strstr(finalVariable, "float ")))
@@ -122,7 +116,6 @@ int main()
                     floatCheck = strstr(floatCheck + 1, "float ");
                     floatCount++;
                 }
-               // printf("floatCount: %d for line[%d]\n", floatCount, i);
             }
 
             if((charCheck = strstr(finalVariable, "char ")))
@@ -133,7 +126,6 @@ int main()
                     charCheck = strstr(charCheck + 1, "char ");
                     charCount++;
                 }
-                //printf("charCount: %d for line[%d]\n", charCount, i);
             }
 
             if((doubleCheck = strstr(finalVariable, "double ")))
@@ -144,7 +136,6 @@ int main()
                     doubleCheck = strstr(doubleCheck + 1, "double ");
                     doubleCount++;
                 }
-                //printf("doubleCount: %d for line[%d]\n", doubleCount, i);
             }
 
             if((shortCheck = strstr(finalVariable, "short ")))
@@ -155,7 +146,6 @@ int main()
                     shortCheck = strstr(shortCheck + 1, "short ");
                     shortCount++;
                 }
-                //printf("shortCount: %d for line[%d]\n", shortCount, i);
             }
 
             if((longCheck = strstr(finalVariable, "long ")))
@@ -166,43 +156,45 @@ int main()
                     longCheck = strstr(longCheck + 1, "long ");
                     longCount++;
                 }
-                //printf("longCount: %d for line[%d]\n", longCount, i);
             }
 
-           // printf("line[%d]: %s", i, fileStorage[i]);
+            printf("line[%d]: %s", i, fileStorage[i]);
         }
-       /* else if(structOpen == 1)
+        /* else if(structOpen == 1)
+           {
+           strcat(store,fileStorage[i]);
+           }*/
+      /* 
+         else if((bracketCheck = strstr(fileStorage[i], "}")) && structOpen == 1 && functionOpen == 1)
         {
-            strcat(store,fileStorage[i]);
-        }*/
-        else if((bracketCheck = strstr(fileStorage[i], "}")) && structOpen == 1 && functionOpen == 1)
-        {
-           strcat(store, fileStorage[i]);
-           printf("store: %s\n", store);    
-           functionOpen--;
-           sprintf(functionArray[functionStorageCount], "%s", store);
-           //strcpy(functionArray[functionStorageCount], store); 
-           functionStorageCount++;
-          // printf("function[%d]: %s", functionStorageCount, functionArray[functionStorageCount]); 
+            
+            strcat(store, fileStorage[i]);
+            printf("store: %s\n", store);    
+            functionOpen--;
+            sprintf(functionArray[functionStorageCount], "%s", store);
+            strcpy(functionArray[functionStorageCount], store); 
+            functionStorageCount++;
+             printf("function[%d]: %s", functionStorageCount, functionArray[functionStorageCount]); 
+             
         }
         else if(structOpen == 1 && functionOpen == 1)
         {
-            //printf("line[%d] copied :%s\n", i, fileStorage[i]);
+            printf("line[%d] copied :%s\n", i, fileStorage[i]);
             strcat(store, fileStorage[i]); 
-        } 
+        }*/ 
         else
         {
-           // printf("line[%d]: %s", i, fileStorage[i]);
+             printf("line[%d]: %s", i, fileStorage[i]);
         }
     }
-      /*  
-    for(int i = 0; i < functionStorageCount; i++)
-    {
+    /*  
+        for(int i = 0; i < functionStorageCount; i++)
+        {
         for(int j = 0; i < functionStorageCount; j++)
         {
-            printf("functionArray[%d][%d]: %c\n", i, j, functionArray[i][j]);
+        printf("functionArray[%d][%d]: %c\n", i, j, functionArray[i][j]);
         }
-    }*/
+        }*/
 
     fclose(fp);
 
