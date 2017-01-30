467ae98ad41294539395cb9be2c4f2cda9c52b0f
Dominick <dominickhera@me.com>
2017-01-30 14:32:41 -0500

    attempting to work on 3110

--- cis3110/a1/my_shell.c
+++ cis3110/a1/my_shell.c
@@ -2,18 +2,27 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
 extern char **getln();
 
-int main() {
+int main() 
+{
 	int i;
 	char **args; 
 	
-	while(1) {
-		args = getln();
-		for(i = 0; args[i] != NULL; i++) {
-			printf("Argument %d: %s\n", i, args[i]);
-		}
-	}
+	while(1) 
+    {
+      args = getln();
+      for(i = 0; args[i] != NULL; i++)
+      {
+        printf("Argument %d: %s\n", i, args[i]);
+        //if(strcmp(args[i], "exit") == 0)
+        //{
+         //   exit(input);
+       // } 
+      }
+    }
 }
 
