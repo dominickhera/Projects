796031e2374ba1a64ae78ceb083bf827064add42
Dominick <dominickhera@me.com>
2017-01-30 23:21:26 -0500

    some commands done, im working as fast as i can rip

--- cis3110/a1/my_shell.c
+++ cis3110/a1/my_shell.c
@@ -9,26 +9,86 @@ extern char **getln();
 
 int main() 
 {
-	int i;
-	char **args; 
-	
-	while(1) 
+    int i;
+    int j;
+    int addInt;
+    int count;
+    char **args; 
+
+    while(1) 
     {
-      args = getln();
-      for(i = 0; args[i] != NULL; i++)
-      {
-        if(i == 0)
+        args = getln();
+        count = 0;
+        for(i = 0; args[i] != NULL; i++)
         {
-            printf("only one arguement entered\n");
-        } 
-         //printf("Argument %d: %s\n", i, args[i]);
-        if(strcmp(args[0], "exit") == 0)
-        {
-           exit(1);
+            count++;
         }
-        
-         
-      }
+
+        for(i = 0; args[i] != NULL; i++)
+        {
+
+            if(count == 1)
+            {
+                if(strcmp(args[i], "exit") == 0)
+                {
+                    exit(1);
+                }
+                else
+                { 
+                    execvp(args[i], args);
+                    // printf("Argument %d: %c\n", i, args[i][i]);
+                    // break;
+                }
+            }
+            else if(count == 2)
+            {
+                if(strpbrk(args[i], "-") == 0)
+                {
+                    // printf("%s\n", args[1]);
+                    execvp(args[0], &args[i]);
+                    // printf("arguement found\n");
+                }
+                // printf("more than one arguement\n");
+                break;
+            }
+            else
+            {
+                if(strcmp(args[i], "add") == 0)
+                {
+                    addInt = 0;
+                    for(j = 1; j < count;j++)
+                    {
+                        addInt += atoi(args[j]);
+                        if(j == count - 1)
+                        {
+                            printf("%s = ", args[j]);
+                        }
+                        else
+                        {
+                            printf("%s + ", args[j]);
+                        }
+                    }
+                    printf("%d\n", addInt);
+                    break;
+
+                }
+                
+                if(strcmp(args[i], "args") == 0)
+                {
+                    printf("argc = %d, args = ", count);
+                    for(j = 1; j < count; j++)
+                    {
+                        if(j == count - 1)
+                        {
+                            printf("%s\n", args[j]);
+                        }
+                        else
+                        {
+                            printf("%s, ", args[j]);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
