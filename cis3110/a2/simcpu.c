a9e5a93d46bceac3e329be74c664abc4b0c7a764
Dominick Hera <dominickhera@me.com>
2017-02-26 13:06:26 -0500

    starting to work on parsin

--- cis3110/a2/simcpu.c
+++ cis3110/a2/simcpu.c
@@ -5,15 +5,36 @@
 int main(int argc, char **argv)
 {
     FILE *fp;
+    char line[256]; 
     char fileName[256];
+    char fileStorage[500][500]; 
+    int detailFlag = 0;
+    int verboseFlag = 0;
+    int roundRobinFlag = 0;
+    int count = 0;   
+ 
 
-    fp = fopen(fileName, "r");  
+    /*if(strcmp(argv[1], "-d") != 0 && strcmp(argv[1], "-r") != 0)
+    {
+      printf("this is argv[2]: %s\n", argv[1]);   
+      //fp = fopen(argv[1], "r");  
+    }
 
-    if(fp == NULL)
+    if(!feof(stdin))
     {
         printf("could not find file, exiting program...\n");
         exit(1);
-    } 
+    }
+   */ 
+    while(fgets(line, sizeof(line), stdin) != NULL)
+    {
+        strcpy(fileStorage[count], line);
+        printf("%s", fileStorage[count]);
+        count++;
+        //printf("%s", line);
+    }
+
+    fclose(fp);
 
     return 0;
 }
