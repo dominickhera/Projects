5b1705825a69136fe8309608d68eda11b2f05e16
Dominick Hera <dominickhera@me.com>
2017-02-26 19:05:41 -0500

    input works

---
+++ cis3110/a2/include/simcpu.h
@@ -0,0 +1,12 @@
+#ifndef __DHERA_SIMCPU__
+#define __DHERA_SIMCPU__
+
+typedef struct cpuProcess
+{
+    int processCount;
+    int threadSwitch;
+    int processSwitch;
+}process;
+
+
+#endif
--- cis3110/a2/src/simcpu.c
+++ cis3110/a2/src/simcpu.c
@@ -1,48 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "simcpu.h"
 
 int main(int argc, char **argv)
 {
-    // FILE *fp;
     char line[256]; 
     char fileName[256];
     char fileStorage[500][500];
     char * parse; 
+    char *p; 
     int detailFlag = 0;
     int verboseFlag = 0;
     int roundRobinFlag = 0;
     int count = 0;   
- 
+    int initCount = 0; 
+    int quantumInteger = 0;
 
-    /*if(strcmp(argv[1], "-d") != 0 && strcmp(argv[1], "-r") != 0)
-    {
-      printf("this is argv[2]: %s\n", argv[1]);   
-      //fp = fopen(argv[1], "r");  
-    }
-
+/*
     if(!feof(stdin))
     {
-        printf("could not find file, exiting program...\n");
-        exit(1);
+    printf("could not find file, exiting program...\n");
+    exit(1);
     }
-   */ 
-    while(fgets(line, sizeof(line), stdin) != NULL)
+     */ 
+
+    for(int i = 0; i < argc; i++)
+    {
+        if(strcmp(argv[i],"-d") == 0)
+        {
+            printf("detail mode enabled\n");
+            detailFlag++;
+        }
+
+        if(strcmp(argv[i], "-v") == 0)
+        {
+            printf("verbose mode enabled\n"); 
+            verboseFlag++;
+        }
+
+        if(strcmp(argv[i], "-r") == 0)
+        {
+            printf("round robin mode enabled\n");
+            roundRobinFlag++;
+            quantumInteger = strtol(argv[i+1], &p, 10);
+            printf("quantum integer is %d\n", quantumInteger); 
+        }
+    }  
+
+ 
+     while(fgets(line, sizeof(line), stdin) != NULL)
     {
         strcpy(fileStorage[count], line);
         // printf("%s", fileStorage[count]);
         count++;
     }
 
-parse = strtok(fileStorage[0], " ");
+    parse = strtok(fileStorage[0], " ");
+    while(parse != NULL)
+    {
+        printf("%s\n", parse);
+        parse = strtok(NULL, " ");
+        initCount++; 
+     }
+    
+  process->processCount = 10;
 
-while(parse != NULL)
-{
-    printf("%s\n", parse);
-    parse = strtok(NULL, " ");
-}
-
-    // fclose(fp);
-
+printf("processCount is %d\n", process.processCount);
+ 
     return 0;
 }
