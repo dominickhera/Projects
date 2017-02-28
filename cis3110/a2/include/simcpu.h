b2326e90fa583c83bc31ed09570277b3aef4099f
Dominick Hera <dominickhera@me.com>
2017-02-27 22:47:49 -0500

    just adding tar file

--- cis3110/a2/include/simcpu.h
+++ cis3110/a2/include/simcpu.h
@@ -1,8 +1,11 @@
 #ifndef __DHERA_SIMCPU__
 #define __DHERA_SIMCPU__
 
+
+//thread struct which is techinically the process struct
 typedef struct Thread
 {
+    int processNum;
 	int threadNum;
 	int arrivalTime;
 	int cpuCount;
@@ -11,6 +14,8 @@ typedef struct Thread
 
 }Thread;
 
+
+//process struct which holds flags and all other basic data
 typedef struct Process
 {
     int processCount;
--- cis3110/a2/src/simcpu.c
+++ cis3110/a2/src/simcpu.c
@@ -7,6 +7,7 @@ int main(int argc, char **argv)
 {
     Process * process = malloc(sizeof(int)* 5000);
 
+    //decalre variables
     char line[256]; 
     char fileStorage[500][500];
     char * parse; 
@@ -16,15 +17,11 @@ int main(int argc, char **argv)
     process->roundRobinFlag = 0;
     int count = 0;   
     int initCount = 0; 
+    int processNumBuffer = 0;
+    int tempProcessNumBuffer = 0; 
 
-    /*
-       if(!feof(stdin))
-       {
-       printf("could not find file, exiting program...\n");
-       exit(1);
-       }
-     */ 
 
+//checks for various flags and initiates them even though they dont completely work.
     for(int i = 0; i < argc; i++)
     {
         // printf("2\n");
@@ -48,12 +45,32 @@ int main(int argc, char **argv)
         }
     }  
 
+
+//takes the textfile and copys it all over into a 2D array
     while(fgets(line, sizeof(line), stdin) != NULL)
     {
         strcpy(fileStorage[count], line);
         count++;
     }
 
+//parses the 2D array and seperates each of the numbers
+// presumable where i wouldve put all the numbers into the struct buttttttt
+    for(int i = 0; i < count; i++)
+    {
+        initCount = 0;
+
+        parse = strtok(fileStorage[i], " ");
+        while(parse != NULL)
+        {
+            printf("%s ", parse);
+            parse = strtok(NULL, " \n");
+            initCount++;
+        }
+        printf("\n");
+    }
+
+
+//specifically parses the first line and stores various peieces into the process struct
     parse = strtok(fileStorage[0], " ");
     while(parse != NULL)
     {
@@ -61,42 +78,60 @@ int main(int argc, char **argv)
         {
             case 0:
                 process->processCount = strtol(parse, &p, 10);
-                printf("Process Count: %d\n", process->processCount);
+                // printf("Process Count: %d\n", process->processCount);
                 break;
             case 1:
                 process->threadSwitch = strtol(parse, &p, 10);
-                printf("Thread Switch: %d\n", process->threadSwitch);
+                // printf("Thread Switch: %d\n", process->threadSwitch);
                 break;
             case 2:
                 process->processSwitch = strtol(parse, &p, 10);
-                printf("Process Switch: %d\n", process->processSwitch);
+                // printf("Process Switch: %d\n", process->processSwitch);
                 break;
             default:
-                printf("default case\n");
+                // printf("default case\n");
                 break;
         }
         parse = strtok(NULL, " \n");
         initCount++; 
     }
 
-    for(int i = 1; i < count; i++)
+
+//speicially parses second line to initiate storing all the other data in the process struct to use for other for loops
+    initCount = 0;
+    parse = strtok(fileStorage[1], " ");
+    while(parse != NULL)
+        // printf("no\n");
     {
-        initCount = 0;
-
-        parse = strtok(fileStorage[i], " ");
-        while(parse != NULL)
+        switch(initCount)
         {
-            printf("line[%d]: %s\n", i, parse);
-            parse = strtok(NULL, " \n");
-            initCount++;
+            case 0:
+                process->cpuThread = malloc(sizeof(process) * 500);
+                processNumBuffer = strtol(parse, &p, 10);
+                // process->cpuThread[0].processNum = strtol(parse, &p, 10);
+                break;
+            case 1:
+                // process->cpuThread = malloc(sizeof(process) * (strtol(parse, &p, 10) * 3));
+                process->cpuThread[0].threadNum = strtol(parse, &p, 10);
+                // printf("Thread Count: %d\n", process->cpuThread[0].threadNum);
+                break;
+            default:
+                break;
         }
+        parse = strtok(NULL, " \n");
+        initCount++; 
     }
-
-    // for(int i = 0; i < process->processCount; i++)
+    // for(int j = 2; i < count; j++)
     // {
-    //     for(int j = 0; j < process->)
+    //     for(int i = 1; i < process->cpuThread[0].threadNum; i++)
+    //     {
+
+    //     }
     // }
 
+
+//ideally, this is where the end data would've been presented
+//but alas it all just ends up as 0 since the simulation is never done.
     if(process->roundRobinFlag != 1)
     {
         printf("\nFCFS: \n\n");
@@ -107,10 +142,10 @@ int main(int argc, char **argv)
     }
 
     printf("Total Time required is %d time units\nAverage Turnaround Time is %d time units\nCPU Utilization is %d%%\n\n", process->totalTime, process->averageTurnAroundTime, process->cpuUtilization);
-    
+
     if(process->detailFlag == 1)
     {
-        printf("butts\n");
+        printf("details should be here but they aren't\n");
     }
 
     return 0;
