6533235c6620958cfc53edb1da99c83733b5275e
Dominick Hera <dominickhera@me.com>
2017-03-07 12:22:14 -0500

    readme and makefile created, had simple file intake for both programs

--- cis3110/a3/src/holes.c
+++ cis3110/a3/src/holes.c
@@ -2,9 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	printf("hi there\n");
+
+	if(argc != 2)
+	{
+		printf("not enough arguments, exiting...\n");
+		exit(1);
+	}
+
+	for(int i = 0; i < argc; i++)
+	{
+		printf("arg[%d]: %s\n", i, argv[i]);
+	}
 
 	return 0;
 }
