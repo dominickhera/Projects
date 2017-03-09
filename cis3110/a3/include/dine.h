201baa9ca77bb98e6f38bc6c386386b499d9c094
dominickhera <dominickhera@me.com>
2017-03-09 11:23:30 -0500

    minor struct put in

---
+++ cis3110/a3/include/dine.h
@@ -0,0 +1,11 @@
+#ifndef __DHERA_DINE__
+#define __DHERA_DINE__
+
+typedef struct Philosopher
+{
+    int philosopherCount;
+    int processCount;
+
+}Philosopher;
+
+#endif
--- cis3110/a3/src/dine.c
+++ cis3110/a3/src/dine.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "dine.h"
+
 
 int main(int argc, char **argv)
 {
+    Philosopher * philosopher = malloc(sizeof(int)* 256);
+    
+    char *p;
 
 	if(argc != 3)
 	{
@@ -11,10 +16,15 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 
-	for(int i = 0; i < argc; i++)
+    philosopher->philosopherCount = strtol(argv[1], &p, 10);
+    philosopher->processCount = strtol(argv[2], &p, 10);
+
+/*	for(int i = 0; i < argc; i++)
 	{
 		printf("arg[%d]: %s\n", i, argv[i]);
 	}
+*/
+    printf("amount of philosophers: %d\namount of times they eat: %d\n", philosopher->philosopherCount, philosopher->processCount);
 
 	return 0;
 }
